9bba8b90e216f773fbdfc972c9d6915f5d025295
jguzmanp695 <jguzman0695@gmail.com>
2021-10-31 16:50:15 -0500

    Productos

---
+++ src/models/income.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Income = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    value: {
+        type: Number,
+        min: 5000,
+        max: 1500000
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//Función que se ejecuta antes de guardar dinero
+Income.pre('save', function(next){
+    if(this.value <= 50000){
+        this.value -= 1000
+    }
+    next()
+})
+
+module.exports = mongoose.model('incomes', Income)
