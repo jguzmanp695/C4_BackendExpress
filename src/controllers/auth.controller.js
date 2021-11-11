91326721cb2666649323f259d35810a168e9fcf1
jguzmanp695 <jguzman0695@gmail.com>
2021-11-11 18:59:24 -0500

    Error de importación service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-const AuthService = require('../models/user')
+const AuthService = require('../services/auth.service')
 const msg = require('../helpers/messages')
 const { validationResult } = require('express-validator')
 const authController = {
